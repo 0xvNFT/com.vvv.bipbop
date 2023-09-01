7c25deb4c40e0c1aa6df19b9586afe68426fe857
Jayson <kanekiken101017@gmail.com>
2023-09-01 18:59:03 +0800

    added timer and modify some elements

--- app/src/main/java/com/vvv/bipbop/IntroActivity.java
+++ app/src/main/java/com/vvv/bipbop/IntroActivity.java
@@ -2,6 +2,7 @@ package com.vvv.bipbop;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,14 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_intro);
 
         ImageView play_now = findViewById(R.id.play_now);
--- app/src/main/java/com/vvv/bipbop/MainActivity.java
+++ app/src/main/java/com/vvv/bipbop/MainActivity.java
@@ -1,6 +1,8 @@
 package com.vvv.bipbop;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +11,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import java.text.MessageFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView home;
@@ -16,18 +19,30 @@ public class MainActivity extends AppCompatActivity {
     private ImageView leaderboard;
     private TextView score;
     private int userScore = 0;
-    private TextView timer;
+    private TextView timerTextView;
+
     private TextView level;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_main);
 
         home = findViewById(R.id.home);
         pause_play = findViewById(R.id.pause_play);
         level = findViewById(R.id.level);
         score = findViewById(R.id.score);
+        timerTextView = findViewById(R.id.timer);
         leaderboard = findViewById(R.id.leaderboard);
+
+        startTimer();
 
         if (savedInstanceState == null) {
             FragmentManager fm = getSupportFragmentManager();
@@ -45,5 +60,28 @@ public class MainActivity extends AppCompatActivity {
     public void currentLevel(int levels) {
         level.setText(MessageFormat.format("Level: {0}", levels));
     }
+
+    private void startTimer() {
+        new CountDownTimer(30000, 1000) { // 30 seconds, tick every 1 second
+            public void onTick(long millisUntilFinished) {
+                // Calculate minutes and seconds from milliseconds
+                long seconds = millisUntilFinished / 1000;
+                long minutes = seconds / 60;
+                seconds = seconds % 60;
+
+                // Format the time as "mm:ss"
+                String time = String.format(Locale.US, "%02d:%02d", minutes, seconds);
+
+                // Update the timer TextView with the formatted time
+                timerTextView.setText(time);
+            }
+
+            public void onFinish() {
+                // Handle timer completion here, e.g., end the game or take appropriate action
+                timerTextView.setText(getString(R.string.timer_text));
+            }
+        }.start();
+    }
+
 
 }
